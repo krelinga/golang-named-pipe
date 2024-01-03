87c44a7fdac1c8cd1deb35b3d9a8bd72ae350ade
Andy Kreling <krelinga@gmail.com>
2024-01-03 12:19:20 -0600

    exit with error if no output is specified.

--- main.go
+++ main.go
@@ -6,5 +6,10 @@ import (
 )
 
 func main() {
+    args := os.Args[1:]
+    if len(args) != 1 {
+        fmt.Println("output file path must be specified.")
+        os.Exit(1)
+    }
     fmt.Println(os.Args[1:])
 }
