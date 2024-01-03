c5d20320259ea54c75932ca1bb4afa867129ba42
Andy Kreling <krelinga@gmail.com>
2024-01-03 12:32:04 -0600

    write a define string to the file passed in argv.

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import (
     "fmt"
     "os"
+    "time"
 )
+
+func checkError(err error) {
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
 
 func main() {
     args := os.Args[1:]
@@ -11,5 +19,11 @@ func main() {
         fmt.Println("output file path must be specified.")
         os.Exit(1)
     }
-    fmt.Println(os.Args[1:])
+    outFilePath := args[0]
+    outFile, err := os.OpenFile(outFilePath, os.O_WRONLY|os.O_SYNC, 0600)
+    checkError(err)
+    outFile.Write([]byte("this is a test\n"))
+    sleepDuration, err := time.ParseDuration("20s")
+    checkError(err)
+    time.Sleep(sleepDuration)
 }
